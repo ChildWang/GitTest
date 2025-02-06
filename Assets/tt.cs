2b263e209008bada4612dbddb3ba7ee72333dc12
ChildWang <2419143831@qq.com>
2025-02-06 22:12:56 +0800

    脚本修改

--- Assets/tt.cs
+++ Assets/tt.cs
@@ -7,12 +7,12 @@ public class tt : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("11");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
